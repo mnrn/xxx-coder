5dd62461f44d60e486a490841c1332deb420acaa
ayase <paper.airplane01@gmail.com>
2020-06-06 10:03:39 +0900

    Update problems.

---
+++ abc069_b.awk
@@ -0,0 +1,1 @@
+$0=substr($0,1,1)(length($0)-2)substr($0,length($0),1)
---
+++ abc156_c.cc
@@ -0,0 +1,44 @@
+// #define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b) for (int i = (a); i < int(b); ++i)
+#define RFOR(i, a, b) for (int i = (b)-1; i >= int(a); --i)
+#define REP(i, n) FOR(i, 0, n)
+#define REP1(i, n) FOR(i, 1, int(n) + 1)
+#define RREP(i, n) RFOR(i, 0, n)
+#define RREP1(i, n) RFOR(i, 1, int(n) + 1)
+#define ALL(c) begin(c), end(c)
+
+int _ = (
+#ifndef LOCAL
+    std::cin.tie(nullptr), std::ios::sync_with_stdio(false),
+#endif
+    std::cout.precision(10), std::cout.setf(std::ios::fixed));
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+template <typename T> using vec = std::vector<T>;
+using namespace std;
+
+constexpr int INF = numeric_limits<int>::max();
+
+int main() {
+  ll N;
+  cin >> N;
+  vec<ll> X(N);
+  REP(i, N) {
+    cin >> X[i];
+  }
+
+  ll answer = INF;
+  REP1(P, 100) {
+    ll c = 0;
+    REP(i, N) {
+      c += (X[i] - P) * (X[i] - P);
+    }
+    answer = min(answer, c);
+  }
+  cout << answer << endl;
+  return 0;
+}
